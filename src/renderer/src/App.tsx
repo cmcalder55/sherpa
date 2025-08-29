bfd38e8b431713e86e2dd216ad9d982a3f46bec1
Cameron Calder <67109992+cmcalder55@users.noreply.github.com>
2025-08-29 18:52:27 -0400

    feat: initialize main application structure with window and preload configurations

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Graph } from '../components/Graph'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div>
+        <Graph />
+      </div>
+    </>
+  )
+}
+
+export default App
